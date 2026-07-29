35d9626a0bb7e6db5dbfc6054a237982a9237457
mahfuja-mansura <mansuramahfuja@gmail.com>
2026-07-29 15:30:37 +0600

    Complete Day 1: Python basics

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("Welcome to my Machine Learning Journey!")
+print("My name is Mahfuja Mansura.")
+
+name = "Mahfuja"
+age = 23
+
+print("Name:", name)
+print("Age:", age)
---
+++ practice1.py
@@ -0,0 +1,12 @@
+name = "Mahfuja"
+print(name)
+uni = "Brac University" 
+print(uni)
+age = 23
+print(age)
+p_lang = "java"
+print(p_lang)
+print(5+7)
+print(5*7)
+print(165)
+print("Hello!\n" + "My name is " + name + "\nI am " + str(age) + " years old." + "\nI study at " + uni + "\nMy favourite programming language is " + p_lang)
